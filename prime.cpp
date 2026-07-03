41d755d53c2626c26f8e25568a60ceecf6b1709f
KOUSHAL VASWANI <koushalvaswani3@gmail.com>
2026-07-03 11:56:05 +0530

    Add files via upload

---
+++ prime.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n < 2) return false;
+    if (n == 2 || n == 3) return true;
+    if (n % 2 == 0) return false;
+
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPrime(num))
+        cout << num << " is a prime number\n";
+    else
+        cout << num << " is NOT a prime number\n";
+
+    return 0;
+}
